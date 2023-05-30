60e598e08be94bdc21f39cfb80024b31d2fdd4e8
Alex Ghiurutan <alexandru.ghiurutan@miele.com>
2023-05-30 17:05:21 +0200

    added form validation and error messages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FindTripComponent } from './components/find-trip/find-trip.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FindTripComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeaderComponent, FindTripComponent],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/find-trip/find-trip.component.ts
+++ src/app/components/find-trip/find-trip.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-find-trip',
   templateUrl: './find-trip.component.html',
-  styleUrls: ['./find-trip.component.css']
+  styleUrls: ['./find-trip.component.css'],
 })
 export class FindTripComponent {
-
+  bookingCode = new FormControl('', [
+    Validators.required,
+    Validators.minLength(5),
+    Validators.maxLength(6),
+    Validators.pattern('^[a-zA-Z 2-9]*$'),
+  ]);
+  familyName = new FormControl('', [
+    Validators.required,
+    Validators.minLength(2),
+    Validators.maxLength(30),
+    Validators.pattern('^[a-zA-Z ]*$'),
+  ]);
 }
